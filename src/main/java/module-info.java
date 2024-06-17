536bf353334ad04709890cd9587a995e7c2e00a2
jerzzy16 <johnjerzz16@gmail.com>
2024-06-17 22:07:02 +0800

    FA1.2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module com.example.fa1 {
+module com.example.fa {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
-    opens com.example.fa1 to javafx.fxml;
-    exports com.example.fa1;
+    opens com.example.fa to javafx.fxml;
+    exports com.example.fa;
+    exports com.example.fa.Controller;
+    opens com.example.fa.Controller to javafx.fxml;
 }
